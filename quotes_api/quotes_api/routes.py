8eb658b69dfd4adb73de42ef8483c58471695706
Barth <juniior.barth@gmail.com>
2019-06-16 19:49:51 -0300

    Add routes for use API

--- quotes_api/quotes_api/routes.py
+++ quotes_api/quotes_api/routes.py
@@ -1,3 +1,6 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('quotes', '/quotes')
+    config.add_route('quote', '/quotes/{quote_number}')
+    config.add_route('random_quote', '/quotes/random/')
