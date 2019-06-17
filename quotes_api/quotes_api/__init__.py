7a573254e47e982d20acaf81f68fa48fa5c4bcfa
Barth <juniior.barth@gmail.com>
2019-06-16 21:53:04 -0300

    Create session log
    close #13

--- quotes_api/quotes_api/__init__.py
+++ quotes_api/quotes_api/__init__.py
@@ -1,10 +1,12 @@
 from pyramid.config import Configurator
+from pyramid.session import SignedCookieSessionFactory
 
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    with Configurator(settings=settings) as config:
+    session_factory = SignedCookieSessionFactory('$2y$12$jBjDCKJ1lU8pLjJyzSnI5uDkul26WHyGvDiODF7xw0/5d4hQJLdn2')
+    with Configurator(settings=settings, session_factory=session_factory) as config:
         config.include('.models')
         config.include('pyramid_jinja2')
         config.include('.routes')
